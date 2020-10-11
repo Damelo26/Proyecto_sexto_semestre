addb0926a9e227fef25f5c32af0311c6eef56406
odsabogal <72479467+odsabogal@users.noreply.github.com>
2020-10-10 21:29:12 -0500

    Añadiendo a inicio
    Añadido nuevas fuentes
    La barra de nav principal
    normalize
    Y codigo de reset

--- Principal.php
+++ Principal.php
@@ -9,7 +9,9 @@ if(!isset($Contenido)){
 <head> 
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="CSS/Estilo.css">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&family=Oswald:wght@200;300;400;500;600;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 	<title>Pets' Home </title>
+	<link rel="stylesheet" href="css/normalize.css"> <!--Esta libreria es importante ya que nos hara que en todo lado el cod se vea igual /Lo normaliza/-->
 </head>
 <body>
 	<div class="header">
@@ -19,9 +21,15 @@ if(!isset($Contenido)){
 	<li ><img src="img/logoprincipal.png" alt=""></li>
 	</figure>
 	</header>
-
-		
-
+	</div>
+	<div class= "menu">
+			<a href="?p=principal">Principal</a>
+			<a href="?p=adopta">Adopta</a>
+			<a href="?p=contactenos">Contactenos</a>
+			<a href="Modulos/Registro_Usuario.php">Registrar usuario</a>
+			<a href="Login.php">Iniciar sesión</a>
+			<a href="Modulos/Exit.php">Salir</a>
+		</div>
 	</div>
 	<!--<nav class="navegacion">
 		<ul class="Menus">
@@ -37,23 +45,17 @@ if(!isset($Contenido)){
 			<li><a href="#">Contacto</a></li>		
 		</ul>
 	</nav>-->
-	<div class= "menu">
-		<a href="?p=principal">Principal</a>
-		<a href="?p=adopta">Adopta</a>
-		<a href="?p=contactenos">Contactenos</a>
-		<a href="Modulos/Registro_Usuario.php">Registrar usuario</a>
-		<a href="Login.php">Iniciar sesión</a>
-		<a href="Modulos/Exit.php">Salir</a>
-	</div>
-	<div class="cuerpo">
-		<?php
-			if(file_exists("Modulos/".$Contenido.".php")){
-				include "Modulos/".$Contenido.".php";
-			}else{
-				echo "<i>No se ha encontrado el modulo <b>".$Contenido."</b> <a href='./'>Regresar</a></i>";
-			}
-		?>
-	</div>
+	<div class="contenedor">
+		<div class="cuerpo">
+			<?php
+				if(file_exists("Modulos/".$Contenido.".php")){
+					include "Modulos/".$Contenido.".php";
+				}else{
+					echo "<i>No se ha encontrado el modulo <b>".$Contenido."</b> <a href='./'>Regresar</a></i>";
+				}
+			?>
+		</div>
+		
 	<div class="footer">
 		Copyright Nombre de la empresa &copy; <?=date("Y")?>
 	</div>	
