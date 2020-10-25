1a582fb5ac08bf29c2e5473554c507c939197720
odsabogal <72479467+odsabogal@users.noreply.github.com>
2020-10-24 20:07:17 -0500

    Revert "Revert "Menu del administrador Header / Footer y menu funcional""
    This reverts commit 28dd6d7ef6a78c8a029995e7916459592d64f271.

---
+++ Adoptado_Admin.php
@@ -0,0 +1,7 @@
+<?php include_once 'Modulos/Templates/Header_Admin.php';  ?>
+<div class="Contenido">
+        <?php
+				echo "Adoptado";
+        ?>
+</div>
+<?php include_once 'Modulos/Templates/Footer_Admin.php';?>
---
+++ Principal_Admin.php
@@ -0,0 +1,7 @@
+<?php include_once 'Modulos/Templates/Header_Admin.php';  ?>
+<div class="Contenido">
+        <?php
+				include "Estadistica_Admin.php";
+        ?>
+</div>
+<?php include_once 'Modulos/Templates/Footer_Admin.php';?>
