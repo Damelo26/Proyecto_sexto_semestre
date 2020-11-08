215d99a34c718b6040add869e38d23b47f647529
Alecz06 <angiiealejandra22@gmail.com>
2020-11-08 13:27:29 -0500

    MANUAL TECNICO INMERSO EN LA APP

---
+++ ManualUsu.php
@@ -0,0 +1,24 @@
+
+<?php include_once 'Modulos/Templates/Header_Admin.php';  ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<style type="text/css">
+#portapdf {
+    
+    width: 1345px;
+    height: 700px;
+    border: 1px solid #484848;
+    margin: 0 auto;
+    
+}
+</style>
+
+
+<body>
+<div id="portapdf">
+    <object data="ManualTec.pdf" type="application/pdf" width="100%" height="100%"></object>
+</div>
+</body>
+
+
+<?php include_once 'Modulos/Templates/Footer_Admin.php';?>
