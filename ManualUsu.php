b7111a7d3984bd3f072f2461f207c13829cd311f
Alecz06 <angiiealejandra22@gmail.com>
2020-11-11 15:06:09 -0500

    MANUAL TECNICO CON NUEVO ER
    Incorporé el nuevo manual técnico con las correcciones que consideré necesarias. Por favor verificar

--- ManualUsu.php
+++ ManualUsu.php
@@ -16,7 +16,7 @@
 
 <body>
 <div id="portapdf">
-    <object data="ManualTec.pdf" type="application/pdf" width="100%" height="100%"></object>
+    <object data="ManualTecnico.pdf" type="application/pdf" width="100%" height="100%"></object>
 </div>
 </body>
 
