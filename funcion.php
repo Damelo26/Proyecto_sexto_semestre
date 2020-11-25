233819fb13864627e1f8f05dd8797893b76d9ef6
odsabogal <72479467+odsabogal@users.noreply.github.com>
2020-11-25 10:56:04 -0500

    Update funcion.php

--- funcion.php
+++ funcion.php
@@ -5,7 +5,8 @@
     }else{
         $id_Mascota = $_REQUEST['id'];
         $id_Usuario = $_REQUEST['usuario'];
-        $query_update = mysqli_query($conexion, "UPDATE mascotas INNER JOIN adoptados Ad ON Ad.ID_Mascotas = mascot.ID_Mascota SET mascotas.ID_Estado = '2' WHERE Ad.ID_Mascotas = '$id_Mascota' AND Ad.Cedula = '$id_Usuario'");
+        $query_update = mysqli_query($conexion, "UPDATE mascotas INNER JOIN adoptados Ad ON Ad.ID_Mascotas = mascotas.ID_Mascota SET mascotas.ID_Estado = '2' WHERE Ad.ID_Mascotas = '$id_Mascota' AND Ad.Cedula = '$id_Usuario'");
+        echo "Prueba";
         if($query_update){
             header('Location: Adoptado_Admin.php');
             echo "Proceso exitoso, mascota aprobada";
